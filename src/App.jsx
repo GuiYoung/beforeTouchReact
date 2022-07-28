59755e7265b106894f98168ceabf2c0ef32928ae
GuiYoung <3530809393@qq.com>
2022-07-28 09:33:31 +0800

    update App.jsx utils.jsx package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import {sculptureList} from "./data";
-import {GetIP,CheckLocation} from "./utils";
+import {CheckLocation} from "./utils";
 import axios from "axios";
 
 // function CheckButton(){
@@ -18,28 +18,31 @@ function DataArea(){
             navigator.geolocation.getCurrentPosition(function (position) {
                 let latitude = position.coords.latitude
                 let longitude = position.coords.longitude
+                console.log(latitude)
+                console.log(longitude)
+                console.log(position.coords.accuracy)
                 axios.get('http://localhost:9001/api/v1/info/uploadInfo', {
                     params: {
                         latitude: latitude,
                         longitude: longitude,
-                        ip: ipAddr,
                     }
                 }).then(function (resp){
                     let wr = resp.data
                     setData(JSON.stringify(wr))
                     okData={}
-                    okData.city=wr.addressComponent.city
-                    okData.direction=wr.addressComponent.direction
-                    okData.distance=wr.addressComponent.distance
-                    okData.district=wr.addressComponent.district
-                    okData.province=wr.addressComponent.province
-                    okData.street=wr.addressComponent.street
-                    okData.street_number=wr.addressComponent.street_number
-                    okData.ip=wr.ip
+                    okData.city=wr.addrInfo.city
+                    okData.direction=wr.addrInfo.direction
+                    okData.distance=wr.addrInfo.distance
+                    okData.district=wr.addrInfo.district
+                    okData.province=wr.addrInfo.province
+                    okData.street=wr.addrInfo.street
+                    okData.street_number=wr.addrInfo.street_number
+                    okData.ip=wr.addrInfo.ip
                     okData.latitude=latitude
                     okData.longitude=longitude
+                    okData.unit_name=wr.addrInfo.unit_name
                 }).catch()
-            }, function (error){console.log({"error":error})})
+            }, function (error){console.log({"error":error})},{enableHighAccuracy: true})
         } else {
             console.log('你的浏览器不支持当前地理位置信息获取')
         }
